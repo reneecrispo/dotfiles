f50864eb1a96512cbbab71112ac48d037e8ca671
Renee Crispo <renee.crispo@gmail.com>
2026-01-28 09:46:47 +0100

    Lazygit keymaps

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -14,15 +14,14 @@ vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<Leader>p', '"+p', {noremap=true, silent=true})
 
 -- Spectre find and replace
-vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
-    desc = "Toggle Spectre"
-})
-vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-    desc = "Search current word"
-})
-vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-    desc = "Search current word"
-})
-vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-    desc = "Search on current file"
-})
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', { desc = "Search current word" })
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', { desc = "Search on current file" })
+
+-- LazyGit
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+vim.keymap.set("n", "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", { desc = "LazyGit current file" })
+vim.keymap.set("n", "<leader>gl", "<cmd>LazyGitLog<cr>", { desc = "LazyGit log" })
+vim.keymap.set("n", "<leader>gb", "<cmd>LazyGitFilter<cr>", { desc = "LazyGit branch filter" })
+vim.keymap.set("n", "<leader>gc", "<cmd>LazyGitFilterCurrentFile<cr>", { desc = "LazyGit commits (file)" })
